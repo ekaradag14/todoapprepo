1dc31a16f4ac693aaa8a0fc64415f7b9a9f95130
Erencan <ekaradag14@ku.edu.tr>
2020-05-11 18:49:23 +0300

    Inıtial commit of Realm Version

---
+++ Todoey/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Erencan Karadağ on 11.05.2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Erencan Karadağ on 11.05.2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
